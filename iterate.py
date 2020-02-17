7ebb5160e5b265a8c16dc890fe21023723839a2b
tings0802 <nick19980922@gmail.com>
2020-02-17 19:01:03 +0800

    pseudocode

--- iterate.py
+++ iterate.py
@@ -1,2 +1,24 @@
 import numpy as np
 import utility as u
+import param as p
+'''
+lapQ = np.(p.mesh_Q)
+
+h_bulk = u.h_bulk(Q)
+h_subs = u.h_surf(Q, p.W_sub)
+h_shel = u.h_surf(Q, p.W_she)
+
+Q += p.dt / p.eta * h_bulk
+Q += p.dt / p.eta * h_subs
+Q += p.dt / p.eta * h_shel
+
+# XZ-periodic boundary
+
+F_bulk = 
+F_subs = 
+F_shel = 
+F_total = F_bulk + F_subs + F_shel
+
+# solve n, S from Q
+
+'''

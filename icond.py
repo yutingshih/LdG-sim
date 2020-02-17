a2c04120369f5a352c702c9930aa35652019ae0f
tings0802 <nick19980922@gmail.com>
2020-02-17 18:59:36 +0800

    initialization, preallocation

--- icond.py
+++ icond.py
@@ -1,2 +1,14 @@
 import numpy as np
 import utility as u
+import param as p
+
+""" initial n, S and Q """
+n0 = u.cartesian(p.n_init, normalize=True)
+S0 = 0.5
+u.Q_tensor(n0, S0)
+
+""" preallocation """
+eig_val = np.zeros(3, 3)
+eig_vec = np.zeros(3, 3)
+N_eig = np.zeros(3)
+S_eig = np.zeros(1)

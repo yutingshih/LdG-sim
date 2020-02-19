ebd80bec7b023f13f5cc95447bf17d088d58973f
tings0802 <nick19980922@gmail.com>
2020-02-20 01:45:34 +0800

    meshgrid objectification

--- bcond.py
+++ bcond.py
@@ -1,6 +1,12 @@
 import numpy as np
-import param as p
 import utility as u
+import param as p
+
+def is_top(position):
+	return False
+
+def is_bot(position):
+	return False
 
 def is_osh(position, scope=p.dx, size=p.r_nog):
 	''' check if a certain position (array-like) is near to the sphere's surface '''
@@ -16,6 +22,7 @@ def is_ish(position, scope=p.dx, size=p.r_nog):
 		return True
 	return False
 
+# logic error
 def anchor(is_surf, trend=u.cartesian(p.n_init)):
 	''' calculate the orientation contributed by the surface anchoring around the sphere '''
 	if is_surf:
@@ -36,10 +43,10 @@ S0_cen = 0.1
 
 # n0_bot = np.array([1, 0, 0])
 # n0_top = np.array([1, 0, 0])
-n0_osh = envelope(osh_mask(p.mesh_Q), u.cartesian(p.n_init))
-n0_ish = envelope(ish_mask(p.mesh_Q), u.cartesian(p.n_init - bias))
+# n0_osh = envelope(osh_mask(p.mesh_Q), u.cartesian(p.n_init))
+# n0_ish = envelope(ish_mask(p.mesh_Q), u.cartesian(p.n_init - bias))
 
 # Q0_bot = u.Q_tensor(n0_bot, S0_sub)
 # Q0_top = u.Q_tensor(n0_top, S0_sub)
-Q0_osh = u.Q_tensor(n0_osh, S0_cen)
-Q0_ish = u.Q_tensor(n0_ish, S0_cen)
+# Q0_osh = u.Q_tensor(n0_osh, S0_cen)
+# Q0_ish = u.Q_tensor(n0_ish, S0_cen)

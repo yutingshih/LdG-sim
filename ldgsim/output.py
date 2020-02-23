61d6897a134da68d5ac435d07450f1f2d909a1b4
tings0802 <nick19980922@gmail.com>
2020-02-23 13:45:36 +0800

    refactor streamline(), add retrive()

--- ldgsim/output.py
+++ ldgsim/output.py
@@ -2,23 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import param as p
+import mesh as m
+import cond as c
 
-def streamline(mesh):
+def retrive_r(grid):
+    ''' retrive the position r from a single grid and store it as a tuple '''
+    if c.is_osh(grid):
+        return grid.r[0], grid.r[1], grid.r[2]
+    else:
+        return 0, 0, 0
+Retrive_r = np.vectorize(retrive_r)
+
+def retrive_n(grid):
+    ''' retrive the orientation n from a single grid and store it as a tuple '''
+    if c.is_ish(grid):
+        return grid.n[0], grid.n[1], grid.n[2]
+    else:
+        return 0, 0, 0
+Retrive_n = np.vectorize(retrive_n)
+
+def streamline(mesh, length=1.0):
     plt.close()
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = plt.figure().add_subplot(111, projection='3d')
 
     x, y, z = np.meshgrid(p.axis_x, p.axis_y, p.axis_z)
-    nx = ny = nz = np.empty(mesh.shape)
+    ry, rx, rz = Retrive_r(mesh)
+    ny, nx, nz = Retrive_n(mesh)
 
-    for i in range(p.x_nog):
-        for j in range(p.y_nog):
-            for k in range(p.z_nog):
-                nx[i, j, k] = mesh[i, j, k].n[0]
-                ny[i, j, k] = mesh[i, j, k].n[1]
-                nz[i, j, k] = mesh[i, j, k].n[2]
-
-    ax.quiver(x, y, z, nx, ny, nz, length=0.8, pivot='middle')
+    ax.quiver(x, y, z, rx, ry, rz, length=length, pivot='middle')
+    # ax.quiver(x, y, z, nx, ny, nz, length=length, pivot='middle', color='red')
 
     plt.show()
+
+
+if __name__ == '__main__':
+    streamline(m.mesh)

c5235cfd95e6bcc6aba5e2e2fea70e9b1b7c7d3b
tings0802 <nick19980922@gmail.com>
2020-02-24 03:19:51 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='ldgsim',
+    version='0.1',
+    description='An implementation of Landau-de Gennes\'s model of nematic liquid crystal simulation',
+    author='YU-TING SHI',
+    author_email='nick19980922@gmail.com',
+    url='https://github.com/tings0802/LdG-sim',
+    packages=['numpy', 'matplotlib'],
+)

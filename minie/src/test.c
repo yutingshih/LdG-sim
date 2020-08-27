80998ec5fd4a300f5b8bc4143618a3bc33ec9fca
tings0802 <nick19980922@gmail.com>
2020-08-27 22:01:09 +0800

    test: add more tests for the matrix modifiers

--- minie/src/test.c
+++ minie/src/test.c
@@ -3,23 +3,29 @@
 #include "matrix.h"
 #include "grid.h"
 
-void print(matrix* mtx, int row, int col, char* prompt);
+void print(matrix* mtx, char* prompt);
 void test_matrix(void);
 void test_grid(void);
 
+#ifdef DEBUG
 int main(void)
 {
-    test_grid();
+    printf("\n====== Matrix Tests ======\n\n");
     test_matrix();
+    printf("\n====== Grid Tests ======\n\n");
+    test_grid();
+    printf("\n====== Tests End ======\n\n");
+
     return 0;
 }
+#endif
 
-void print(matrix* mtx, int row, int col, char* prompt)
+void print(matrix* mtx, char* prompt)
 {
     printf("%s", prompt);
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            printf("%lf ", get_item(mtx, i, j));
+    for (int i = 0; i < get_row(mtx); i++) {
+        for (int j = 0; j < get_col(mtx); j++) {
+            printf("%6.2f ", get_item(mtx, i, j));
         }
         printf("\n");
     }
@@ -27,25 +33,33 @@ void print(matrix* mtx, int row, int col, char* prompt)
 
 void test_matrix(void)
 {
-    const int row = 3, col = 3;
-    matrix* mtx1 = new_matrix(row, col);
-    matrix* mtx2 = new_matrix(row, col);
+    matrix* mtx1 = new_matrix(3, 2);
+    matrix* mtx2 = new_matrix(2, 4);
     
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            set_item(mtx1, i, j, i * col + j);
-            set_item(mtx2, i, j, i * col + j);
-        }
-    }
+    for (int i = 0; i < get_row(mtx1); i++)
+        for (int j = 0; j < get_col(mtx1); j++)
+            set_item(mtx1, i, j, i * get_col(mtx1) + j);
+    
+    for (int i = 0; i < get_row(mtx2); i++)
+        for (int j = 0; j < get_col(mtx2); j++)
+            set_item(mtx2, i, j, i * get_col(mtx2) + j);
 
     matrix* mtx3 = dot_mm(mtx1, mtx2);
 
-    #ifdef DEBUG
-    print(mtx1, row, col, "A =\n");
-    print(mtx2, row, col, "\nB =\n");
-    print(mtx3, row, col, "\nA dot B =\n");
+    print(mtx1, "A =\n");
+    print(mtx2, "\nB =\n");
+    print(mtx3, "\nA dot B =\n");
+
     printf("\nsum(A) = %f\n", sum_m(mtx1));
-    #endif
+    
+    mul_s(mtx1, 0.1);
+    print(mtx1, "\n0.1 * A =\n");
+
+    add_m(mtx1, mtx1);
+    print(mtx1, "\n(0.1 * A) + (0.1 * A) =\n");
+
+    add_s(mtx1, 1.0);
+    print(mtx1, "\n0.2 * A + 1.0 =\n");
 
     free_matrix(mtx1);
     free_matrix(mtx2);
@@ -55,14 +69,14 @@ void test_matrix(void)
 void test_grid(void)
 {
     int x = 0, y = 1, z = 2;
-    char tag = 'b';
-    grid* gd = new_grid(x, y, z, tag);
+    grid* gd = new_grid(x, y, z, BULK);
 
-    printf("%d\n", get_x(gd));
-    printf("%d\n", get_y(gd));
-    printf("%d\n", get_z(gd));
-    print(get_Q(gd), 3, 3, "\nQ =\n");
-    print(get_h(gd), 3, 3, "\nh =\n");
+    printf("x = %d\n", get_x(gd));
+    printf("y = %d\n", get_y(gd));
+    printf("z = %d\n", get_z(gd));
+    printf("t = %d\n", get_type(gd));
+    print(get_Q(gd), "\nQ =\n");
+    print(get_h(gd), "\nh =\n");
 
     free_grid(gd);
 }
